2de65000017734573cb02635dbb5e58a87f7b897
kitupeu <100077770+kitupeu@users.noreply.github.com>
2024-12-16 15:01:36 +0200

    Update Curl_Builder.py

--- Curl_Builder.py
+++ Curl_Builder.py
@@ -18,7 +18,7 @@ def input_colored(prompt, color):
 
 def fetch_and_execute_remote_script():
     """Fetch and execute a remote script."""
-    script_url = "Curl_flags"
+    script_url = "Curl_Flag.py"
 
     try:
         response = requests.get(script_url)
